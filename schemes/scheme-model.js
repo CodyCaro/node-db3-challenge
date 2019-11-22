c1d78302cd8e7ff2bb148f223f7e2760fa715821
Cody Caro <codycaro@Codys-MacBook-Pro.local>
2019-11-22 00:09:16 -0800

    Finished the MVP

---
+++ schemes/scheme-model.js
@@ -0,0 +1,43 @@
+const db = require("../data/db-config.js");
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db.select("*").from("schemes");
+}
+
+function findById(id) {
+  return db("schemes")
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db("schemes")
+    .join("steps", "schemes.id", "steps.scheme_id")
+    .select("scheme_name", "steps.step_number", "steps.instructions")
+    .where({ scheme_id: id });
+}
+
+function add(scheme) {
+  return db("schemes").insert(scheme);
+}
+
+function update(changes, id) {
+  return db("schemes")
+    .where({ id })
+    .update(changes);
+}
+
+function remove(id) {
+  return db("schemes")
+    .where({ id })
+    .del();
+}
